4edbdb955fe33a49bf31388e9cd5df154d6d9a3d
sebasG101 <sebastian.guerra01@epn.edu.ec>
2024-11-01 20:08:58 -0500

    Testeo de integracion en Jenkins

--- src/main/java/service/ComentarioService.java
+++ src/main/java/service/ComentarioService.java
@@ -14,7 +14,7 @@ public class ComentarioService {
         comentario.setFechaCreacion(new Date());
         comentario.setUsuario(usuario);
         comentario.setResena(resena);
-        resena.getComentarios().add(comentario); // Asegura que el comentario se añade a la reseña
+        //resena.getComentarios().add(comentario); // Asegura que el comentario se añade a la reseña
         return comentario;
     }
 }
