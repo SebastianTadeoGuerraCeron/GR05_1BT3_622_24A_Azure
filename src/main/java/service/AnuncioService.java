45d41170ec2bfa4def67fb2e10679b5b8de5f90d
Aizen003 <david.quille@epn.edu.ec>
2024-11-02 21:36:36 -0500

    IT2_HU09_T1_T2
    Autor: David Quille
    Se hizo tarea 1 y 2, agregando el respectivo jsp.

---
+++ src/main/java/service/AnuncioService.java
@@ -0,0 +1,15 @@
+package service;
+
+import modelo.Anuncio;
+import modelo.Usuario;
+
+//import java.util.Date;
+//import java.util.List;
+//import java.util.stream.Collectors;
+
+public class AnuncioService {
+
+    public Anuncio crearAnuncio(String nombreRestaurante, String tipoComida, String ubicacion, String descripcionOfertas, Usuario usuario) {
+       return null;
+    }
+}
